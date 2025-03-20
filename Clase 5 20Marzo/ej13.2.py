34a91f113cd257900878a1b7ad144bfd8119e701
Juli Rossney <rossneyjulieta@gmail.com>
2025-03-20 15:48:07 -0300

    quinto commit juli

---
+++ Clase 5 20Marzo/ej13.2.py
@@ -0,0 +1,13 @@
+color_alien = "verde"
+if color_alien == "verde":
+    print ("Ganaste 5 puntos")
+else:
+    print ("") 
+
+print("")
+print("version2")
+color_alien = "rojo"
+if color_alien == "verde":
+    print ("Ganaste 5 puntos")
+else:
+    print ("") 
---
+++ Clase 5 20Marzo/ej13.3.py
@@ -0,0 +1,42 @@
+print ("version1")
+color_alien = "amarillo"
+if color_alien == "verde":
+    print ("Ganaste 5 puntos")
+elif color_alien == "amarillo":
+    print ("ganaste 10 puntos") 
+elif color_alien == "rojo":
+    print ("15 puntos")
+else:
+    print ("0 puntos")
+
+print ("version2")
+color_alien = "verde"
+if color_alien == "verde":
+    print ("Ganaste 5 puntos")
+elif color_alien == "amarillo":
+    print ("ganaste 10 puntos") 
+elif color_alien == "rojo":
+    print ("15 puntos")
+else:
+    print ("0 puntos")
+
+print ("version3")
+if color_alien == "rojo":
+    print ("Ganaste 5 puntos")
+elif color_alien == "amarillo":
+    print ("ganaste 10 puntos") 
+elif color_alien == "rojo":
+    print ("15 puntos")
+else:
+    print ("0 puntos")
+
+print ("version4")
+color_alien = "violeta"
+if color_alien == "verde":
+    print ("Ganaste 5 puntos")
+elif color_alien == "amarillo":
+    print ("ganaste 10 puntos") 
+elif color_alien == "rojo":
+    print ("15 puntos")
+else:
+    print ("0 puntos")
