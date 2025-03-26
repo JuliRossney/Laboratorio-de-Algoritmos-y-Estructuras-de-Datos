a974dcc3af1864212b2d82fded1867a1bfdebd6f
Rocio <rpodazza@alumno.huergo.edu.ar>
2025-03-26 09:28:57 -0300

    sexto commit juli

---
+++ Clase 6 26Marzo/ej 13.4.py
@@ -0,0 +1,13 @@
+edad = 14
+if edad < 2:
+    print ("es un bebé")
+elif edad >= 2 and edad < 4:
+    print ("es un nene/a chiquito/a")
+elif edad >= 4 and edad < 13: 
+    print ("es un nene/a")
+elif edad >= 13 and edad < 20:
+    print ("es un adolescente")
+elif edad >= 20 and edad < 65:
+    print ("es un adulto")
+elif edad >= 65:
+    print ("es un adulto mayor")
