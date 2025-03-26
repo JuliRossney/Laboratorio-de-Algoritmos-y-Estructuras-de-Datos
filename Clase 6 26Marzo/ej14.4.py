4c0e2018bf5bf7690f238f15eb59ba9790ea5019
Rocio <rpodazza@alumno.huergo.edu.ar>
2025-03-26 11:47:12 -0300

    sexto commit juli

---
+++ Clase 6 26Marzo/ej14.4.py
@@ -0,0 +1,12 @@
+numeros = [1,2,3,4,5,6,7,8,9]
+for numero in numeros:
+    if numero == 1 or numero == 3:
+        print(f"{numero}ero")
+    elif numero == 2:
+        print(f"{numero}ndo")
+    elif numero == 4 or numero == 5 or numero == 6:
+        print(f"{numero}to")  
+    elif numero == 8:
+        print(f"{numero}vo")
+    elif numero == 9:
+        print(f"{numero}no")
