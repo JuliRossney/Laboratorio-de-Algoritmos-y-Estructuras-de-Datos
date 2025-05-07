da6ef19abbb372690843d01d8ca43cda1aa67060
Juli Rossney <rossneyjulieta@gmail.com>
2025-05-07 09:33:41 -0300

    noveno commit juli

---
+++ Clase 8 3Abril/ej22.1.py
@@ -0,0 +1,5 @@
+def hacer_camiseta (talla, mensaje):
+    print(f"Se ha hecho una camiseta de talla {talla} con el mensaje: {mensaje}")
+
+hacer_camiseta("M", "Juli")
+hacer_camiseta(talla="L", mensaje="Hola")
