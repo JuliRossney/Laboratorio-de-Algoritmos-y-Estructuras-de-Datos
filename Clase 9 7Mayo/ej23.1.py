f42b1edb24a4547f258a6e7732dcbf22ff160979
Juli Rossney <rossneyjulieta@gmail.com>
2025-05-08 13:55:40 -0300

    noveno commit juli

--- Clase 9 7Mayo/ej23.1.py
+++ Clase 9 7Mayo/ej23.1.py
@@ -1,6 +1,5 @@
-def describir_ciudad(ciudad, pais):
-    print(f"{ciudad} está en {pais}.")
-
-describir_ciudad("Buenos Aires", "Argentina")
-describir_ciudad("Madrid", "España")
-describir_ciudad("Lima", "Perú")
+def ciudad_pais(ciudad, pais):
+    return f"{ciudad}, {pais}."
+print(ciudad_pais("Buenos Aires", "Argentina"))
+print(ciudad_pais("Lima", "Perú"))
+print(ciudad_pais("Rosario", "Argentina"))
